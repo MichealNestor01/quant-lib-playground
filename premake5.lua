ca2dff585a492eb590e7f7053138eba53b67d2a9
Micheal Nestor <michealnestor@outlook.com>
2024-05-23 00:32:07 +0100

    Added zero yield bond calculation from vasily nekrasov's getting started guide

--- premake5.lua
+++ premake5.lua
@@ -26,3 +26,27 @@ project "testApp"
     filter "configurations:Release"
         defines { "NDEBUG" }
         optimize "On"
+
+project "zeroBondYield"
+    kind "ConsoleApp"
+    language "C++"
+    targetdir "bin/%{cfg.buildcfg}"
+    files { "src/zeroBondYield/**.h", "src/zeroBondYield/**.cpp" }
+    
+    includedirs { "src/libraries/QuantLib/include" }
+    libdirs { "src/libraries/QuantLib/lib" }
+
+    links { "QuantLib" }
+
+    -- This is needed to make the linker find the QuantLib library
+    linkoptions { "-Wl,-rpath=src/libraries/QuantLib/lib" }
+    system "linux"
+    toolset "gcc"
+
+    filter "configurations:Debug"
+        defines { "DEBUG" }
+        symbols "On"
+
+    filter "configurations:Release"
+        defines { "NDEBUG" }
+        optimize "On"
