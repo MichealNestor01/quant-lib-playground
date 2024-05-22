029beeaea6451045716ccb3269948cad26df2261
Micheal Nestor <michealnestor@outlook.com>
2024-05-23 00:25:01 +0100

    added date test from Vadily Nekrasov's getting started book

--- src/testApp/test.cpp
+++ src/testApp/test.cpp
@@ -1,8 +1,21 @@
-#include <ql/quantlib.hpp>
 #include <iostream>
+#include <ql/time/date.hpp>
 
-int main() {
-    QuantLib::Date d(1, QuantLib::January, 2015);
-    std::cout << "The day of the week on 1st January 2015 was: " << d.weekday() << std::endl;
+
+int main()
+{
+    std::cout << std::endl << "===Test QuantLib===" << std::endl;
+
+    QuantLib::Date veryFirstDate(1, QuantLib::January, 1901);
+    QuantLib::Date anotherVeryFirstDate(367);
+    QuantLib::Date yetAnotherVeryFirstDate(veryFirstDate);
+    std::cout << (QuantLib::Date(10, QuantLib::December, 1979)).weekday() << std::endl;
+
+    std::cout << veryFirstDate << std::endl;
+    std::cout << anotherVeryFirstDate << std::endl;
+    std::cout << yetAnotherVeryFirstDate << std::endl;
+
+    std::cout << QuantLib::Date::minDate() << std::endl;
+    std::cout << QuantLib::Date::maxDate() << std::endl;
     return 0;
 }
